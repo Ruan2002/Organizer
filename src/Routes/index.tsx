0c4354903dda4232788fd3479dbcce6e5567b86c
Ruan Ladwig Lima <132952987+Ruan2002@users.noreply.github.com>
2024-02-01 16:08:32 -0300

    algumas alterações e estilização

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import { Home } from '../pages/Home'
 import {Notifications} from '../pages/Notifications'
 import {Profile} from '../pages/Profile'
@@ -11,14 +11,28 @@ export function Routes(){
     return(
         <Tab.Navigator
         screenOptions={{
-            headerShown: false
+            headerShown: false,
+            tabBarShowLabel: false,
+
+            tabBarStyle:{
+                backgroundColor: '#D3D3D3',
+                borderWidth: 1
+            },
+
+            tabBarActiveTintColor: '#FFF'
         }}
-        
-        
         >
             <Tab.Screen
               name='Home'
               component={Home}
+              options={{
+                tabBarIcon: ({ focused, size, color }) => {
+                    if (focused) {
+                      return <Ionicons name="home" size={size} color={color} />;
+                    }
+                    return <Ionicons name="home-outline" size={size} color={color} />;
+                  }
+              }}
             />
             <Tab.Screen
               name='Notifications'
